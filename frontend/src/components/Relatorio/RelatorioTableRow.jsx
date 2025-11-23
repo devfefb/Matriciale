9b2ee0820bfbac984db5deb586c66b95d247f41d
gustavobm <g.bianchinimoraes@gmail.com>
2025-11-23 15:26:25 -0300

    fix: create display correct null data in a table row.

--- frontend/src/components/Relatorio/RelatorioTableRow.jsx
+++ frontend/src/components/Relatorio/RelatorioTableRow.jsx
@@ -8,10 +8,10 @@ const RelatorioTableRow = ({ relatorios }) => {
             <td><div className={styles.item}>{relatorios.cod_item}</div></td>
             <td><div className={styles.item}>{relatorios.nome_item}</div></td>
             <td><div className={styles.item}>{relatorios.tp_metodo}</div></td>
-            <td><div className={styles.item}>{relatorios.metodo ? relatorios.metodo.toLocaleString('pt-BR') : '-'}</div></td>
-            <td><div className={styles.item}>{relatorios.met_est ? relatorios.met_est.toLocaleString('pt-BR') : '-'}</div></td>
+            <td><div className={styles.item}>{relatorios.metodo ? relatorios.metodo.toLocaleString('pt-BR') : '0'}</div></td>
+            <td><div className={styles.item}>{relatorios.met_est ? relatorios.met_est.toLocaleString('pt-BR') : '0'}</div></td>
             <td><div className={styles.item}>{relatorios.estoque ? relatorios.estoque.toLocaleString('pt-BR') : '0'}</div></td>
-            <td><div className={styles.item}>{relatorios.reposicao ? relatorios.reposicao.toLocaleString('pt-BR') : '-'}</div></td>
+            <td><div className={styles.item}>{relatorios.reposicao ? relatorios.reposicao.toLocaleString('pt-BR') : '0'}</div></td>
         </tr>
     )
 }
